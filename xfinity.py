8625c6db3df4f5e9bd5c0b92ccb86a6f387bc0b9
Dustin Roberts <dustinroberts96@gmail.com>
2019-10-25 19:51:45 -0400

    added exception handling
    added exception handling for my networking functions.

--- xfinity.py
+++ xfinity.py
@@ -13,7 +13,8 @@ import subprocess
 import random
 import string 
 from tqdm import tqdm
-
+import sys
+import os
 
 
 
@@ -27,39 +28,81 @@ def random_email():
 	return(USERNAME, EMAIL)
 
 
-def reset_wifi():
-	subprocess.call(["sudo nmcli c del id xfinitywifi", stdout=DEVNULL], shell=True)
-	subprocess.call(["sudo ifconfig wlp3s0 down",], shell=True)
+def change_mac(pbar):
+	subprocess.call(["sudo nmcli c del id xfinitywifi"], stderr=open(os.devnull, 'wb'), stdout=open(os.devnull, 'wb'), shell=True)
+	subprocess.call(["sudo ifconfig wlp3s0 down"], shell=True)
 	time.sleep(1)
 	try:
-		subprocess.call(["sudo macchanger -r wlp3s0", stdout=DEVNULL], shell=True)
-	except:
-		time.sleep(5)
-		subprocess.call(["sudo macchanger -r wlp3s0"], stdout=DEVNULL, shell=True)
-
-	time.sleep(2)
-	try:
-		subprocess.call(["sudo ifconfig wlp3s0 up"], shell=True)
-	except:
-		subprocess.call(["sudo rfkill unblock wifi; sudo rfkill unblock all"], shell=True)
-		time.sleep(1)
-	time.sleep(5)
-	subprocess.call(["nmcli d wifi connect xfinitywifi"], shell=True)
+		subprocess.call(["sudo macchanger -r wlp3s0"], stderr=open(os.devnull, 'wb'), stdout=open(os.devnull, 'wb'), shell=True)
+	except OSError:
+		time.sleep(3)
+		try:
+			subprocess.call(["sudo macchanger -r wlp3s0"],stderr=open(os.devnull, 'wb'), stdout=open(os.devnull, 'wb'), shell=True)
+		except OSError:
+			subprocess.call(["sudo service network-manager restart"], shell=True)
+			time.sleep(10)
+			main()
 	
+	pbar.update(5)
+
+def connect_wifi(pbar)
+	wifi_up = subprocess.call(["sudo ifconfig wlp3s0 up"], stderr=open(os.devnull, 'wb'), shell=True)
+	if wifi_up != 0:
+		#troubleshooting network issues
+		#try unblocking wifi card and if that does work restart wifi service
+		subprocess.call(["sudo rfkill unblock wifi; sudo rfkill unblock all"], shell=True)
+		time.sleep(2)
+		wifi_up = subprocess.call(["sudo ifconfig wlp3s0 up"],stderr=open(os.devnull, 'wb'), shell=True)
+		if wifi_up != 0:
+			subprocess.call(["sudo service network-manager restart"], shell=True)
+			print("-----restarting------")
+			time.sleep(10)
+			#subprocess.call("sudo systemctl start networking")
+			#time.sleep(5)
+			main()
+		else:
+			pbar.update(10)
+
+	else:
+		pbar.update(10)
+
+	connect = subprocess.call(["nmcli d wifi connect xfinitywifi"], stderr=open(os.devnull, 'wb'),stdout=open(os.devnull, 'wb'), shell=True)
+	if connect !=0:
+		time.sleep(5)
+		connect = subprocess.call(["nmcli d wifi connect xfinitywifi"], stderr=open(os.devnull, 'wb'),stdout=open(os.devnull, 'wb'), shell=True)
+		if connect !=0:
+			subprocess.call(["sudo service network-manager restart"], shell=True)
+			print("     Can't connect to WiFi")
+			print("-----restarting------")
+			time.sleep(10)
+			#subprocess.call(["sudo systemctl start networking"])
+			#time.sleep(5)
+			main()
+		else:
+			pbar.update(20)
+	else:
+		pbar.update(20)
 
 
-
-def signup():
-	USERNAME, EMAIL = random_email()
-	#setup
+def setup(pbar):
 	capa = DesiredCapabilities.CHROME
 	capa["pageLoadStrategy"] = "none"
 	options = Options()
 	options.add_argument('--headless')
 	chromedriver = 'chromedriver'
 	driver = webdriver.Chrome(desired_capabilities=capa, options=options)
-	time.sleep(10)
+	
+	driver.implicitly_wait(60)
 	driver.get('http://10.232.0.1')
+	pbar.update(10)
+	return(driver)
+
+
+def signup(pbar):
+
+	USERNAME, EMAIL = random_email()
+	driver = setup(pbar)
+
 	#page1
 	driver.implicitly_wait(60)
 	b = driver.find_element_by_id("amdocs_signup")
@@ -75,14 +118,14 @@ def signup():
 	driver.implicitly_wait(60)
 	n = driver.find_element_by_id("upgradeOfferCancelButton")
 	n.click()
-	#page 3 form
-	#page
+	#page 3 
 	capa = DesiredCapabilities.CHROME
 	capa["pageLoadStrategy"] = "none"
 
 
 	#wait.until(EC.presence_of_element_located((By.ID, '#dk0-combobox')))
-	time.sleep(5)
+	pbar.update(20)
+	time.sleep(3)
 	driver.execute_script("window.stop();")
 	first =  driver.find_element_by_name("firstName")
 	last = driver.find_element_by_name("lastName")
@@ -109,14 +152,22 @@ def signup():
 	#page4
 	#wait to initialize need to chage to see when spinner stops
 	#wait.until(EC.presence_of_element_located((By.ID, '_orderConfirmationActivatePass')))
+	pbar.update(20)
 
 	driver.implicitly_wait(60)
 	activate_pass = driver.find_element_by_id("_orderConfirmationActivatePass")
 	activate_pass.click()
+	pbar.update(20)
+
+def main():
+	pbar = tqdm(total=100) 
+	change_mac(pbar)
+	time.sleep(2)
+	connect_wifi()
+	time.sleep(1)
+	signup(pbar)
+	pbar.close()
 	print("You got internet bitch!")
 
 
-
-reset_wifi()
-signup()
-
+main()
